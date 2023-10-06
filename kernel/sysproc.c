d96ccdbf81bf78883865aa2bd55afe92cdaf830f
firesoul956 <FireSoul956@gmail.com>
2023-10-06 15:22:01 -0600

    just doing a routine save.

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -93,6 +93,46 @@ sys_uptime(void)
 int
 sys_pages(void)
 {
+//get the pid from arguments
+  int pid;
+  struct cpu *c=mycpu();
+  argint(0, &pid);
+
+  
+
+//with the pid, find p
+  int found = 0;
+  struct proc *my_p;
+  struct proc *proc;
+  struct proc *p;
+  uint64 pt[NPROC];
+
+  for( p = proc; p < &proc[NPROC]; p++) {
+      if (p->pid == pid) {
+          found = 1;
+          my_p = p;
+          break;
+      }
+  }
+
+//loop through the pagetable
+  pt = my_p->pagetable;
 
 return -1;
+}
+
+//i is what page we are looking at (I.E what number in what layer)
+//depth is how far we are down, max of 3.
+//list is the return of the flags and the physical location
+uint64 rec_page(i,depth,list){
+  uint64 j=i;
+  while(j, j<512,j++ ){
+    if (j &&1){
+      if (depth==3){
+        
+      }
+      rec_page(j,depth+1,list);
+    }
+
+  }
 }

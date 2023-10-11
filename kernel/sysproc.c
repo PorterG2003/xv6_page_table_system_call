021de2d3e6024f5cbb2947031d225e1c64f6a5b6
PorterG2003 <porterg2003@gmail.com>
2023-10-11 15:44:13 -0600

    more stuff

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -143,36 +143,30 @@ sys_pages(void)
 
 int rec_page(int i, int depth){
   uint64 j=i;
-  while(j<512){
-    //if (j || 1111111111111110  == 11111111111111111){
-    if(j & PTE_V){
-      if (depth==3){
+  while (j<512) {
+    if (j & PTE_V) {
+      if (depth==3) {
         //read
-         //if (j || 1111111111111101  != 11111111111111111){
-         if(j & PTE_R){
-	   printf("!r");
-         }
-         //write
-         //if (j || 1111111111111011  != 11111111111111111){
-	 if(j & PTE_W){
+        if (j & PTE_R) {
+          printf("!r");
+        }
+              //write
+        if (j & PTE_W) {
           printf("!w");
-         }
-         //execute
-         //if (j || 1111111111110111  != 11111111111111111){
-	 if(j & PTE_X){
+        }
+              //execute
+        if (j & PTE_X) {
           printf("!x");
-         }
-         //user accesable
-         //if (j || 1111111111101111  != 11111111111111111){
-	 if(j & PTE_U){
+        }
+              //user accesable
+        if (j & PTE_U) {
           printf("!u");
-         }
+        }
         return 0;
       }
       printf("j");
       rec_page(j,depth+1);
     }
-
   }
   return 0;
 }
